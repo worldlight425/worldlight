ee26be8cfb90477ff51781c1553cf3255634921f
worldlight425 <worldlight0425@gmail.com>
2021-10-06 23:10:26 +0200

    adds 404NotFound screen component

---
+++ project/src/components/not-found-screen/not-found-screen.tsx
@@ -0,0 +1,38 @@
+function NotFoundScreen(): JSX.Element {
+  return (
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <h1 className="page-title user-page__title">404</h1>
+      </header>
+
+      <div className="sign-in user-page__content">
+        <h1>OOOOPS! Page not Found</h1>
+        <p>The page you are looking for has been moved or doesn’t exist anymore, if you like you can return to our <a href="/">homepage</a>.</p>
+      </div>
+
+      <footer className="page-footer">
+        <div className="logo">
+          <a href="main.html" className="logo__link logo__link--light">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
+        </div>
+
+        <div className="copyright">
+          <p>© 2019 What to watch Ltd.</p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default NotFoundScreen;
