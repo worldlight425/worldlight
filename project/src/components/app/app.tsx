b87227680fdcb8ea06b61d9ddaf2adc17b589ed6
worldlight425 <worldlight0425@gmail.com>
2021-10-06 23:19:47 +0200

    imports BrowserRouter, Switch, Route

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import MainScreen from '../main-screen/main-screen';
+import PlayerScreen from '../player-screen/player-screen';
+import SignInScreen from '../sign-in-screen/sign-in-screen';
+import MyListScreen from '../my-list-screen/my-list-screen';
+import FilmScreen from '../film-screen/film-screen';
+import AddReviewScreen from '../add-review-screen/add-review-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 interface AppScreenProps {
   title: string,
