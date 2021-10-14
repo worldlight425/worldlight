0f518123e329689f6c1a817d4828b759c33fc239
worldlight425 <worldlight0425@gmail.com>
2021-10-14 09:42:38 +0200

    imports mocks in index.tsx and passes props to App. WIP

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/app/app';
+import {films} from 'mocks/films';
 
 const PromoFilm = {
   title: 'The Grand Budapest Hotel',
@@ -14,6 +15,7 @@ ReactDOM.render(
       title={PromoFilm.title}
       genre={PromoFilm.genre}
       year={PromoFilm.year}
+      films={films}
     />
   </React.StrictMode>,
   document.getElementById('root'));
