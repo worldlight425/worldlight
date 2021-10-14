d49e01b81d6ab532ab2f7df6d7c3cb8013bd0320
worldlight425 <worldlight0425@gmail.com>
2021-10-14 12:37:28 +0200

    adds state to film list component. WIP

--- project/src/components/films-list/films-list.tsx
+++ project/src/components/films-list/films-list.tsx
@@ -1,3 +1,4 @@
+import {useState} from 'react';
 import {Films} from 'types/film';
 import FilmCard from 'components/film-card/film-card';
 
@@ -8,11 +9,13 @@ type FilmComponentProps = {
 function FilmsList(props: FilmComponentProps): JSX.Element {
   const {films} = props;
 
+  const [activeFilm, setActiveFilm] = useState(0);
+
   return (
-    <div className="catalog__films-list">
-      {films.map((film) => {
+    <div className="catalog__films-list" data-active-film={activeFilm} onMouseOver={() => setActiveFilm(1)}>
+      {films.map((film, index) => {
         return (
-          <FilmCard id={film.id} previewImage={film.previewImage} name={film.name}/>
+          <FilmCard id={film.id} previewImage={film.previewImage} name={film.name} />
         )
       })}
     </div>
