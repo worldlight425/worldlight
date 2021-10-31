950cb4b70f5e5a4498164611ad3323f561e027ca
worldlight425 <worldlight0425@gmail.com>
2021-11-01 00:28:18 +0100

    adds Redux

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from 'components/app/app';
 import {promoFilm, films} from 'fixtures/films';
 import {comments} from 'fixtures/comments';
+import {reducer} from 'store/reducer';
+
+const store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App promoFilm={promoFilm} films={films} comments={comments} />
+    <Provider store={store}>
+      <App promoFilm={promoFilm} films={films} comments={comments} />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
