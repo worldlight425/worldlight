cb53ce37b7885d8ba08d3e959c4c0c457141eac8
worldlight425 <worldlight0425@gmail.com>
2021-11-11 13:11:51 +0100

    adds adapter fo server films

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,9 +1,12 @@
-import {ThunkActionResult} from 'types/action';
 import {loadFilms} from 'store/action';
 import {APIRoute} from 'configs/routes';
-import {Films} from 'types/film';
+import {ThunkActionResult} from 'types/action';
+import {ServerFilm} from 'types/film';
+import {adaptFilmToClient} from 'services/adapters';
 
-export const fetchFilmsAction = (): ThunkActionResult => async (dispatch, _getState, api): Promise<void> => {
-  const {data} = await api.get<Films>(APIRoute.Films);
-  dispatch(loadFilms(data));
-};
+export const fetchFilmsAction = (): ThunkActionResult =>
+  async (dispatch, _getState, api): Promise<void> => {
+    const {data: serverFilms} = await api.get(APIRoute.Films);
+    const filmsData = serverFilms.map((serverFilm: ServerFilm) => adaptFilmToClient(serverFilm));
+    dispatch(loadFilms(filmsData));
+  };
--- project/src/types/film.ts
+++ project/src/types/film.ts
@@ -30,3 +30,23 @@ export interface Film {
   isFavorite: boolean;
 }
 export type Films = Film[];
+
+export interface ServerFilm {
+  id: number;
+  name: string;
+  'poster_image': string;
+  'preview_image': string;
+  'background_image': string;
+  'background_color': string;
+  'video_link': string;
+  'preview_video_link': string;
+  description: string;
+  rating: number;
+  'scores_count': number;
+  director: string;
+  starring: string[];
+  'run_time': number;
+  genre: GenreName;
+  released: number;
+  'is_favorite': boolean;
+}
